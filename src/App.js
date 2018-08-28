c5c00f2b44b3205fbf0401ad9cd980c858a58354
Igor Kleponosov <ikleponosov@webcodin.com>
2018-08-28 14:16:58 +0300

    for home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './global/Header/Header';
 import Footer from './global/Footer/Footer';
+import Layout from './global/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Layout />
         <Footer />
       </div>
     );
--- src/global/Layout/Layout.js
+++ src/global/Layout/Layout.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import TestData from './../../components/TestData/TestData';
+import { Container } from 'reactstrap';
 
 const Layout = (props) => {
   return (
     <div className="Layout">
-      {props.children}
+      {/* {props.children} */}
+      <Container>
+        <TestData />
+      </Container>
     </div>
   );
 };
